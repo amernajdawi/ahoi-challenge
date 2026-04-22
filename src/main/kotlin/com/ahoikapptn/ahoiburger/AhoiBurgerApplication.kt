04daaf51c7b4df6a59438c39c40e2579411faa62
amernajdawi <amernajdawi8@gmail.com>
2026-04-22 21:57:24 +0200

    feat: add API key security scheme to Swagger UI

--- src/main/kotlin/com/ahoikapptn/ahoiburger/AhoiBurgerApplication.kt
+++ src/main/kotlin/com/ahoikapptn/ahoiburger/AhoiBurgerApplication.kt
@@ -1,7 +1,11 @@
 package com.ahoikapptn.ahoiburger
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.config.EnableMongoAuditing
@@ -13,7 +17,14 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing
         title = "Ahoi Burger API",
         version = "1.0",
         description = "REST API for managing burgers and drinks"
-    )
+    ),
+    security = [SecurityRequirement(name = "X-API-Key")]
+)
+@SecurityScheme(
+    name = "X-API-Key",
+    type = SecuritySchemeType.APIKEY,
+    `in` = SecuritySchemeIn.HEADER,
+    paramName = "X-API-Key"
 )
 class AhoiBurgerApplication
 
